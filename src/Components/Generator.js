041fbc4acdf147a1f9df115aed5544c928ee6be9
siamr <siam_1000@hotmail.com>
2023-01-11 14:36:00 -0500

    aesthetic and logic changes

--- src/Components/Generator.js
+++ src/Components/Generator.js
@@ -4,12 +4,16 @@ import '../Stylesheets/Generator.scss';
 
 function Generator() {
     const [quote, setQuote] = useState("");
+    const [displayQuote, setDisplayQuote] = useState(false);
 
     const newQuote = async () => {
         try{
             const response = await fetch('https://api.quotable.io/random');
             const quote = await response.json();
+            const displayQuote = () => setDisplayQuote(true);
+
             setQuote(quote);
+            setDisplayQuote(displayQuote);
         }catch(error){
             console.log(error);
         }
@@ -18,11 +22,13 @@ function Generator() {
     return (
         <div className="generator">
             <div className="quote-container">
-                <p>
-                    "{quote.content}"
+                <p className="text">
+                    {displayQuote ? quote.content : "Click the button below to generate a new quote."}
                 </p>
-                <h4>- {quote.author}</h4>
-                <button onClick={newQuote}>New Quote</button>
+                <h4 className="text">
+                    {displayQuote ? "- " + quote.author : ""}
+                </h4>
+                <button className="gen-btn" onClick={newQuote}>New Quote</button>
             </div>
         </div>
     )
